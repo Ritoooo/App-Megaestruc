a8ac6ca7c6e725d3a7ccb1864f1a4369e1b15a39
Ritoooo <ritorito.141516@gmail.com>
2019-06-03 18:09:33 -0500

    añado listado clientes

--- app/src/main/java/com/megaestruc/megaestruc_app/MainActivity.java
+++ app/src/main/java/com/megaestruc/megaestruc_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.megaestruc.megaestruc_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void irCoti(View view){
+        Intent objCoti = new Intent(this,Cotizacion.class);
+        startActivity(objCoti);
+    }
+    public void irProd(View view){
+        Intent objCoti = new Intent(this,Producto.class);
+        startActivity(objCoti);
+    }
+    public void irClie(View view){
+        Intent objCoti = new Intent(this,Clientes.class);
+        startActivity(objCoti);
     }
 }
